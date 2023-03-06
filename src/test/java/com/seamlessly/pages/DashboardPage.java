dd7958e29ea80b596fd2c1e7d7cbfccab58800bc
Ashley Kucuk <ashleyskucuk@gmail.com>
2023-03-06 12:32:42 -0500

    changed base page name to dashboard for readability

--- src/test/java/com/seamlessly/pages/DashboardPage.java
+++ src/test/java/com/seamlessly/pages/DashboardPage.java
@@ -8,18 +8,11 @@ import com.seamlessly.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.CacheLookup;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.List;
+public abstract class DashboardPage {
 
-public abstract class BasePage {
-
-    public BasePage() {
+    public DashboardPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
