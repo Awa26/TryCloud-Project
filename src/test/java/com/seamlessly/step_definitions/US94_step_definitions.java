cbaa4f33da814149ee9c05c2037e26a59de65604
Ashley Kucuk <ashleyskucuk@gmail.com>
2023-03-06 13:35:06 -0500

    renamed

--- src/test/java/com/seamlessly/step_definitions/US94_step_definitions.java
+++ src/test/java/com/seamlessly/step_definitions/US94_step_definitions.java
@@ -7,7 +7,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class Files_step_definitions {
+public class US94_step_definitions {
 
 
     // ashley US  As a user, I should be able to update settings.
@@ -19,12 +19,13 @@ public class Files_step_definitions {
     public void user_on_the_dashboard_page() {
        Driver.getDriver().get(ConfigurationReader.getProperty("dashboardURL"));
     }
-    @When("the user clicks the files module")
-    public void the_user_clicks_the_module(String filesIcon) {
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module() {
         dashboardPage.filesIcon.click();
     }
     @When("user clicks Settings on the left bottom corner")
     public void user_clicks_settings_on_the_left_bottom_corner() {
+
 
     }
     @Then("the user should be able to click any checkbox")
--- src/test/resources/features/US94.feature
+++ src/test/resources/features/US94.feature
@@ -6,6 +6,6 @@ Feature:  As a user, I should be able to update settings
 
 
   Scenario: Verify the users can check any checkbox on the Files page
-    When the user clicks the "Files" module
+    When the user clicks the Files module
     And user clicks Settings on the left bottom corner
     Then the user should be able to click any checkbox
