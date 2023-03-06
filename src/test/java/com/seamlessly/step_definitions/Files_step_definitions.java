860eeda97c6ef84dd6a3d241fe434a023ec2b8b8
Ashley Kucuk <ashleyskucuk@gmail.com>
2023-03-06 13:18:57 -0500

    created files implementation page

---
+++ src/test/java/com/seamlessly/step_definitions/Files_step_definitions.java
@@ -0,0 +1,44 @@
+package com.seamlessly.step_definitions;
+
+import com.seamlessly.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Files_step_definitions {
+
+    // ashley US  As a user, I should be able to update settings.
+
+    @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
+        Driver.getDriver()
+    }
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user clicks Settings on the left bottom corner")
+    public void user_clicks_settings_on_the_left_bottom_corner() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should be able to click any checkbox")
+    public void the_user_should_be_able_to_click_any_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
