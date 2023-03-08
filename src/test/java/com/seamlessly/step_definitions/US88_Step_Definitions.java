1fca21fdedf02bbf709e5f424ebba48d522b6857
zubex_Cydeo <marko_zubex@hotmail.com>
2023-03-08 13:21:44 -0500

    Added new locator and passed testing

--- src/test/java/com/seamlessly/step_definitions/US88_Step_Definitions.java
+++ src/test/java/com/seamlessly/step_definitions/US88_Step_Definitions.java
@@ -4,6 +4,7 @@ import com.seamlessly.pages.FilesPage;
 import com.seamlessly.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class US88_Step_Definitions {
 
@@ -17,12 +18,12 @@ public class US88_Step_Definitions {
     @When("users uploads file with the upload file option")
     public void users_uploads_file_with_the_upload_file_option() {
         //filesPage.uploadFileButton.click();
-        filesPage.uploadFileButton.sendKeys("/Users/ivayloklisarski/Desktop/Upload.txt");
+        filesPage.uploadFileButton.sendKeys("C:\\Users\\marko\\OneDrive\\Skrivbord\\Marko Test File.txt");
         //BrowserUtils.sleep(2);
     }
     @Then("verify the file is displayed on the page")
     public void verify_the_file_is_displayed_on_the_page() {
-
+        Assert.assertTrue(filesPage.uploadedFile.isDisplayed());
     }
 
 }
