0f9d08f00b312709b17be09306b8e8f789200f4d
berhan <berhanh23@gmail.com>
2023-03-09 22:13:18 -0500

    Berhan added some files

--- src/test/java/com/seamlessly/runners/CukesRunner.java
+++ src/test/java/com/seamlessly/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         glue = "com/seamlessly/step_definitions",
         dryRun = false,
 
-        tags = "@ac",
+        tags = "",
        // tags = "@dashboard",
 
         publish = true //generating a report with public link
